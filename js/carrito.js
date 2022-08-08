8714f5a3628ec5970ca451e293d20857b5e49be6
ignacioborraz <ignacioborraz@hotmail.com>
2022-08-07 23:44:02 -0300

    mrs wines index

--- js/carrito.js
+++
@@ -1,5 +0,0 @@
-let ids = []
-if (localStorage.getItem('carrito')) { //si existe el carrito en el almacenamiento
-  ids = JSON.parse(localStorage.getItem('carrito')) //guardo los ids del storage
-  //console.log(ids)
-}
