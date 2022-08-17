cf3dfb8d933bee8ae890fe42948025dea3eed8fb
ignacioborraz <ignacioborraz@hotmail.com>
2022-08-17 11:35:44 -0300

    agregado almacenamiento de favoritos

--- js/almacenamiento.js
+++ js/almacenamiento.js
@@ -4,3 +4,12 @@ if (localStorage.getItem('productos')) { //si existe el almacenamiento
     //busco la clave con getItem
     //una vez que la encuentra: transformo el json en objeto/array
 }
+
+//Definir un almacenamiento para el storage como el creado para productos
+
+let productosDeFavoritos = [] //defino los productos
+if (localStorage.getItem('favoritos')) { //si existe el almacenamiento
+    productosDeFavoritos = JSON.parse(localStorage.getItem('favoritos')) //lo cargo
+    //busco la clave con getItem
+    //una vez que la encuentra: transformo el json en objeto/array
+}
